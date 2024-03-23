5394ca82102d3187619f7f3438cd4c1eac4d34d4
vamshidhar1128 <164383145+vamshidhar1128@users.noreply.github.com>
2024-03-23 23:12:10 +0530

    Create provider.tf

---
+++ test/provider.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    type = string
+    #default = "vamsi-cloud" 
+}
+
+variable "compute_instance_name" {
+    type = string
+    default = "jenkins-vm"
+
+
+}
+variable "allow_stopping" {
+    type = bool
+
+  
+}
+
